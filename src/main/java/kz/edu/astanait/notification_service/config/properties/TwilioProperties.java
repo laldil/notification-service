53daee56b793d93710d179603fe652e3bcede38f
Aldiyar <aldiar001122@gmail.com>
2024-09-04 02:53:11 +0500

    FEAT: implement SMS sender

---
+++ src/main/java/kz/edu/astanait/notification_service/config/properties/TwilioProperties.java
@@ -0,0 +1,16 @@
+package kz.edu.astanait.notification_service.config.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "settings.twilio")
+public class TwilioProperties {
+    private String accountSid;
+    private String authToken;
+    private String number;
+}
--- src/main/java/kz/edu/astanait/notification_service/serivce/impl/notification/SmsNotificationSender.java
+++ src/main/java/kz/edu/astanait/notification_service/serivce/impl/notification/SmsNotificationSender.java
@@ -1,14 +1,22 @@
 package kz.edu.astanait.notification_service.serivce.impl.notification;
 
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import kz.edu.astanait.notification_service.config.properties.TwilioProperties;
 import kz.edu.astanait.notification_service.model.ReceiverContactEntity;
 import kz.edu.astanait.notification_service.model.enums.ContactType;
 import kz.edu.astanait.notification_service.serivce.NotificationSender;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
+@RequiredArgsConstructor
 @Service
 public class SmsNotificationSender implements NotificationSender {
+
+    private final TwilioProperties twilioProperties;
+
     @Override
     public ContactType getType() {
         return ContactType.SMS;
@@ -16,10 +24,11 @@ public class SmsNotificationSender implements NotificationSender {
 
     @Override
     public void sendNotification(ReceiverContactEntity contact, String message) {
-        log.info("""
-                Contacts: %s
-                Message: %s
-                """.formatted(contact.getContact(), message));
-
+        var sms = Message.creator(
+                new PhoneNumber(contact.getContact()),
+                new PhoneNumber(twilioProperties.getNumber()),
+                message
+        ).create();
+        log.info("Sent message to {}", sms.getTo());
     }
 }
